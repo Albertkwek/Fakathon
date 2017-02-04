dbf8d0453b3f25b1851685bf7687e6fbcc7b9e59
Albert Kwek <albert.kwek@columbia.edu>
2017-02-03 23:47:30 -0500

    updated APIs for html_to_factors

--- html_to_factors.py
+++ html_to_factors.py
@@ -10,13 +10,13 @@ def html_to_factors_nytimes(url):
     if response.status_code == 200:
         page_data_soup = BeautifulSoup(response.content,'lxml')
         author_name = page_data_soup.find('span', class_='byline-author').text
-        num_links = len(page_data_soup.find_all('a', href=True))
+        links = [link.get('href') for link in page_data_soup.find_all('a', href=True)]
         headline = page_data_soup.find('h1', itemprop="headline").text
         len_headline = len(headline)
         body = ''
         for tag in page_data_soup.find_all('p', class_='story-body-text story-content'):
             body += tag.text
-        dict_solution = {'author_name':author_name, "num_links":num_links, "headline":headline,"body":body}
+        dict_solution = {'author_name':author_name, "links":links, "headline":headline,"body":body}
         return dict_solution
 
 def html_to_factors_21cent(url):
@@ -31,10 +31,10 @@ def html_to_factors_21cent(url):
     if response.status_code == 200:
         page_data_soup = BeautifulSoup(response.content,'lxml')  
         author_name = page_data_soup.find('a', rel='author').text
-        num_links = len(page_data_soup.find_all('a', href=True))
+        links = [link.get('href') for link in page_data_soup.find_all('a', href=True)]
         headline = page_data_soup.find('h1', class_="entry-title").text
         body = ''
         for tag in page_data_soup.find_all('p'):
             body += tag.text
-        dict_solution = {'author_name':author_name, "num_links":num_links, "headline":headline,"body":body}
+        dict_solution = {'author_name':author_name, "links":links, "headline":headline,"body":body}
         return dict_solution
