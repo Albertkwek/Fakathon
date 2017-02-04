995f91e284b834d3f1b62ba187e1610ac7519115
Albert Kwek <albert.kwek@columbia.edu>
2017-02-04 02:21:06 -0500

    Updated to_factors.py

--- html_to_factors.py
+++ html_to_factors.py
@@ -8,16 +8,19 @@ def html_to_factors_nytimes(url):
     
     response = requests.get(url)
     if response.status_code == 200:
-        page_data_soup = BeautifulSoup(response.content,'lxml')
-        author_name = page_data_soup.find('span', class_='byline-author').text
-        links = [link.get('href') for link in page_data_soup.find_all('a', href=True)]
-        headline = page_data_soup.find('h1', itemprop="headline").text
-        len_headline = len(headline)
-        body = ''
-        for tag in page_data_soup.find_all('p', class_='story-body-text story-content'):
-            body += tag.text
-        dict_solution = {'author_name':author_name, "links":links, "headline":headline,"body":body}
-        return dict_solution
+        try:
+            page_data_soup = BeautifulSoup(response.content,'lxml')
+            author_name = page_data_soup.find('span', class_='byline-author').text
+            links = [link.get('href') for link in page_data_soup.find_all('a', href=True)]
+            headline = page_data_soup.find('h1', itemprop="headline").text
+            len_headline = len(headline)
+            body = ''
+            for tag in page_data_soup.find_all('p', class_='story-body-text story-content'):
+                body += " "*(len(body)!=0) + tag.text
+            dict_solution = {'author_name':author_name, "links":links, "headline":headline,"body":body, "url":url}
+            return dict_solution
+        except:
+            return None
 
 def html_to_factors_21cent(url):
     """
@@ -29,12 +32,15 @@ def html_to_factors_21cent(url):
     
     response = requests.get(url)
     if response.status_code == 200:
-        page_data_soup = BeautifulSoup(response.content,'lxml')  
-        author_name = page_data_soup.find('a', rel='author').text
-        links = [link.get('href') for link in page_data_soup.find_all('a', href=True)]
-        headline = page_data_soup.find('h1', class_="entry-title").text
-        body = ''
-        for tag in page_data_soup.find_all('p'):
-            body += tag.text
-        dict_solution = {'author_name':author_name, "links":links, "headline":headline,"body":body}
-        return dict_solution
+        try:
+            page_data_soup = BeautifulSoup(response.content,'lxml')  
+            author_name = page_data_soup.find('a', rel='author').text
+            links = [link.get('href') for link in page_data_soup.find_all('a', href=True)]
+            headline = page_data_soup.find('h1', class_="entry-title").text
+            body = ''
+            for tag in page_data_soup.find_all('p'):
+                body += " "*(len(body)!=0) + tag.text
+            dict_solution = {'author_name':author_name, "links":links, "headline":headline,"body":body, "url":url}
+            return dict_solution
+        except:
+            return None
